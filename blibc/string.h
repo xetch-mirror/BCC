9034b80e6abfd95815f3f6a496f52c637bb54e7d
Xetch <babyroselegaspi01@gmail.com>
2026-07-30 18:35:36 +0800

    Update string.h

--- blibc/string.h
+++ blibc/string.h
@@ -4,24 +4,44 @@
 
 #include "stddef.h"
 
-// implementing 
-char *strcpy(char *dest, char *src) {
-    char *saved = dest;
+int strlen(int *str) {
+    int *p;
+    p = str;
+    while (*p != 0) {
+        p = p + 1;
+    }
+    return p - str;
+}
+
+int strcmp(int *s1, int *s2) {
+    while (*s1 != 0 && *s1 == *s2) {
+        s1 = s1 + 1;
+        s2 = s2 + 1;
+    }
+    return *s1 - *s2;
+}
+
+int *strcpy(int *dest, int *src) {
+    int *saved;
+    saved = dest;
     while (*src != 0) {
         *dest = *src;
         dest = dest + 1;
         src = src + 1;
     }
-    *dest = 0; // term 
+    *dest = 0;
     return saved;
 }
 
-size_t strlen(char *str) {
-    char *p = str;
-    while (*p != 0) {
+int *memset(int *ptr, int val, int num) {
+    int *p;
+    p = ptr;
+    while (num > 0) {
+        *p = val;
         p = p + 1;
+        num = num - 1;
     }
-    return p - str;
+    return ptr;
 }
 
 #endif
