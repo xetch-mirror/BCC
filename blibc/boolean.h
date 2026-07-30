a7c3c64ac51294c211c81a7e25eaaa7180f27e1e
Xetch <babyroselegaspi01@gmail.com>
2026-07-30 18:42:42 +0800

    Create boolean.h

---
+++ blibc/boolean.h
@@ -0,0 +1,16 @@
+// bool.h
+
+#ifndef BOOL.H
+#define BOOL.H
+
+// bool is int?
+#define bool int
+#define true 1
+#define false 0
+// true or false
+
+#define right 1
+#define wrong 0
+// this is right or wrong
+
+#endif
