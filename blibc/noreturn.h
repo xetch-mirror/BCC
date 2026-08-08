8607542cdc744691539247307609308985b38b85
Xetch <babyroselegaspi01@gmail.com>
2026-08-08 16:13:38 +0800

    Delete noreturn.h

--- blibc/noreturn.h
+++
@@ -1,9 +0,0 @@
-// noreturn.h
-#ifndef noreturn
-    #if defined(__GNUC__) || defined(__clang__)
-        #define noreturn __attribute__((__noreturn__))
-    #else
-        #define noreturn _Noreturn
-    #endif
-    #define __noreturn_is_defined 1
-#endif
